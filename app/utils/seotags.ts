efc97518a60d47270e4cbb654e42992682329c16
Conor <conor@visibleone.com>
2023-08-07 09:15:42 +0800

    Utils: Change Imgur Image and SiteURL :white_check_mark:

--- app/utils/seotags.ts
+++ app/utils/seotags.ts
@@ -3,13 +3,13 @@ const blogDescription =
   "The most recent developments in the technological world, including the best (and worst) gadgets, software, and more. Daily updates on what's important in the world of technology, from industry giants like Google and Apple to scrappy newcomers.";
 const blogKeywords =
   "technology, gadgets, software, news, tech news, Chen Lay, CL, Web Developer, Web Innovations, Web Design, Web Development, Web, Web Developer in Myanmar, Web Developer in Yangon, Web Developer in Mandalay, Web Developer in Naypyitaw, Web Developer in Bago, Web Developer in Magway, Web Developer in Mon, Web Developer in Rakhine, Web Developer in Shan, Web Developer in Kayah, Web Developer in Kayin, Web Developer in Chin, Web Developer in Sagaing, Web Developer in Ayeyarwady, Web Developer in Tanintharyi, Web Developer in Kachin";
-const blogImageUrl = "https://i.imgur.com/DAHohhG.png";
+const blogImageUrl = "https://i.imgur.com/aGcCRuz.png";
 
-const siteUrl = "https://chanmyaemaung.net";
+const siteUrl = "https://chenlay.vercel.app";
 const siteAuthor = "Chen Lay";
 const sitePublisher = "Chen Lay";
 
-const canonicalUrl = "https://chanmyaemaung.net";
+const canonicalUrl = "https://chenlay.vercel.app";
 const twitterHandle = "@itchanlay";
 
 export {
